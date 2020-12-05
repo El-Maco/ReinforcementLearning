c0a22ac6c123b38f12667e7825bcb2976bbde6b2
Robin Nyman <robin.nyman@aalto.fi>
2020-12-05 15:31:10 +0200

    Added Final agent

---
+++ agent/agent.py
@@ -0,0 +1,170 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+from torch.distributions import Normal
+import numpy as np
+import random
+from Todo_utilis import Transition, ReplayMemory
+
+
+# https://pytorch.org/tutorials/intermediate/reinforcement_q_learning.html
+# The DQN implementation was later changed to this:
+# https://ai.stackexchange.com/questions/10203/dqn-stuck-at-suboptimal-policy-in-atari-pong-task
+class DQN(nn.Module):
+
+    def __init__(self, actions, frames):
+        super(DQN, self).__init__()
+        self.action_space = actions
+        self.hidden = 512
+
+        self.conv1 = torch.nn.Conv2d(in_channels=frames,
+                                     out_channels=32,
+                                     kernel_size=8,
+                                     stride=4)
+        self.conv2 = torch.nn.Conv2d(32, 64, 4, 2)
+        self.conv3 = torch.nn.Conv2d(64, 64, 3, 1)
+        self.reshaped_size = 64 * 9 * 9
+        self.fc1 = torch.nn.Linear(self.reshaped_size, self.hidden)
+        self.fc2 = torch.nn.Linear(self.hidden, actions)
+
+        # Number of Linear input connections depends on output of conv2d layers
+        # and therefore the input image size, so compute it.
+
+    # Called with either one element to determine next action, or a batch
+    # during optimization. Returns tensor([[left0exp,right0exp]...]).
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+        x = F.relu(self.conv3(x))
+        x = x.reshape(x.shape[0], self.reshaped_size)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
+
+
+# Agent from exercise 4
+class Agent(object):
+    def __init__(self, n_actions, frame_stacks, gamma, batch_size, replay_buffer_size):
+        self.train_device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        print("train_device is:", self.train_device)
+        self.policy_net = DQN(n_actions,frame_stacks).to(device=self.train_device)
+        self.target_net = DQN(100, 100, 2).to(device=self.train_device)
+        self.target_net.load_state_dict(self.policy_net.state_dict())
+        self.target_net.eval()
+        self.optimizer = optim.RMSprop(self.policy_net.parameters(), lr=1e-4, eps=1e-2, momentum=0.0)
+        self.gamma = gamma
+        self.states = []
+        self.memory = ReplayMemory(replay_buffer_size)
+        self.batch_size = batch_size
+        self.prev_obs = None
+        self.n_actions = 3
+        self.frame_stacks = 3
+        self.agent_name = "Todo"  # Pronounced similar to Frodo
+
+    def update_network(self, updates=1):
+        for _ in range(updates):
+            self._do_network_update()
+
+    def _do_network_update(self):
+        if len(self.memory) < self.batch_size:
+            return
+        transitions = self.memory.sample(self.batch_size)
+        # Transpose the batch (see https://stackoverflow.com/a/19343/3343043 for
+        # detailed explanation). This converts batch-array of Transitions
+        # to Transition of batch-arrays.
+        batch = Transition(*zip(*transitions))
+
+        # Compute a mask of non-final states and concatenate the batch elements
+        # (a final state would've been the one after which simulation ended)
+        non_final_mask = 1 - torch.tensor(batch.done, dtype=torch.uint8)
+        non_final_mask = non_final_mask.type(torch.bool)
+        non_final_next_states = [s for nonfinal, s in zip(non_final_mask,
+                                                          batch.next_state) if nonfinal > 0]
+        non_final_next_states = torch.stack(non_final_next_states).to(self.train_device)
+        state_batch = torch.stack(batch.state).to(self.train_device)
+        action_batch = torch.cat(batch.action).to(self.train_device)
+        reward_batch = torch.cat(batch.reward).to(self.train_device)
+        # columns of actions taken. These are the actions which would've been taken
+        # for each batch state according to policy_net
+        state_action_values = self.policy_net(state_batch).gather(1, action_batch)
+
+        # Compute V(s_{t+1}) for all next states.
+        # Expected values of actions for non_final_next_states are computed based
+        # on the "older" target_net; selecting their best reward with max(1)[0].
+        # This is merged based on the mask, such that we'll have either the expected
+        # state value or 0 in case the state was final.
+        next_state_values = torch.zeros(self.batch_size).to(device=self.train_device)
+
+        # Double Dqn implementation from
+        # https://github.com/Shivanshu-Gupta/Pytorch-Double-DQN/blob/master/agent.py
+        _, next_state_actions = self.policy_net(non_final_next_states).max(1, keepdim=True)
+        next_state_values[non_final_mask] = self.target_net(non_final_next_states).gather(1,
+                                                                                          next_state_actions).squeeze(1)
+
+        # Task 4: TODO: Compute the expected Q values
+        expected_state_action_values = reward_batch + self.gamma * next_state_values
+
+        # Compute Huber loss
+        loss = F.smooth_l1_loss(state_action_values.squeeze(),
+                                expected_state_action_values)
+
+        # Optimize the model
+        self.optimizer.zero_grad()
+        loss.backward()
+        for param in self.policy_net.parameters():
+            param.grad.data.clamp_(-1e-1, 1e-1)
+        self.optimizer.step()
+
+    def get_action(self, state, epsilon=0.05):
+        state = self._preprocess(state)
+        sample = random.random()
+        if sample > epsilon:
+            with torch.no_grad():
+                # Have to convert to a batch of one state!
+                state = torch.from_numpy(state).float().unsqueeze(0).to(device=self.train_device)
+                q_values = self.policy_net(state)
+                return torch.argmax(q_values).item()
+        else:
+            return random.randrange(self.n_actions)
+
+    def update_target_network(self):
+        self.target_net.load_state_dict(self.policy_net.state_dict())
+
+    def store_transition(self, state, action, next_state, reward, done):
+        action = torch.Tensor([[action]]).long()
+        reward = torch.tensor([reward], dtype=torch.float32)
+        next_state = torch.from_numpy(next_state).float()
+        state = torch.from_numpy(state).float()
+        self.memory.push(state, action, next_state, reward, done)
+
+    def load_model(self, fpath):
+        # Pass filename as argument to load desired model
+        return torch.load(fpath)
+
+    def get_name(self):
+        return self.name
+
+    def reset(self):
+        return
+
+    # https://becominghuman.ai/lets-build-an-atari-ai-part-1-dqn-df57e8ff3b26
+    def _preprocess(self, observation):
+
+        observation = np.mean(observation, axis=2).astype(np.uint8)  # convert to greyscale
+
+        observation = observation[::2, ::2] # Downsample with half
+        observation = np.expand_dims(observation, axis=0)
+        if self.prev_obs is None:
+            self.prev_obs = observation
+
+        stack_ob = np.concatenate((self.prev_obs, observation), axis=0)
+
+        # Frame stacking
+        while stack_ob.shape[0] < self.frame_stacks:
+            stack_ob = self._stack_frames(stack_ob, observation)
+        self.prev_obs = stack_ob[1:self.frame_stacks, :, :]
+        return stack_ob
+
+    def _stack_frames(self, stack_ob, obs):
+        return np.concatenate((stack_ob, obs), axis=0)
---
+++ agent/train_agent.py
@@ -0,0 +1,136 @@
+import wimblepong
+import gym
+import numpy as np
+from matplotlib import pyplot as plt
+from itertools import count
+import torch
+import logging
+import sys
+import os
+import argparse
+import random
+from agent import Agent as DQNAgent
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--load", action="store_true",
+                    help="Load weights from file")
+parser.add_argument("--file", type=str, default=None,
+                    help="Name of the file that contains the weights")
+parser.add_argument("--render", action="store_true",
+                    help="render")
+args = parser.parse_args()
+
+# set up logging
+logging.basicConfig(level=logging.INFO, filename='dqn.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s')
+logging.info("log file for DQN agent training")
+
+# Make the environment
+env_name = "WimblepongVisualSimpleAI-v0"
+# env_name = "CartPole-v0"
+env = gym.make(env_name)
+
+if "CartPole" in env_name:
+    TARGET_UPDATE = 50
+    glie_a = 500
+    num_episodes = 2000
+    hidden = 12
+    gamma = 0.95
+    replay_buffer_size = 500000
+    batch_size = 256
+    frame_stacks = 2
+    height = 400
+    width = 600
+elif "WimblepongVisualSimpleAI" in env_name:
+    TARGET_UPDATE = 30
+    glie_a = 5000
+    num_episodes = 50000
+    hidden = 64
+    gamma = 0.99
+    replay_buffer_size = 50000
+    batch_size = 32
+    frame_stacks = 2
+    height = 100
+    width = 100
+else:
+    raise ValueError("Please provide hyperparameters for %s" % env_name)
+
+
+wins = 0
+
+# Get number of actions from gym action space
+n_actions = env.action_space.n
+state_space_dim = env.observation_space.shape
+
+
+# Task 4 - DQN
+agent = DQNAgent(n_actions, frame_stacks, gamma, batch_size, replay_buffer_size)
+if args.load:
+    agent.policy_net.load_state_dict(agent.load_model(args.file))
+
+# Training loop
+#FROM EXERCISE 4
+cumulative_rewards = []
+for ep in range(num_episodes):
+    # Initialize the environment and state
+    state = env.reset()
+
+    # PRE PROCESS THE STATE
+    if "CartPole" in env_name:
+        state = env.render(mode='rgb_array')
+        env.close()
+    state = agent._preprocess(state)
+
+    done = False
+    if ep < 25000:
+    	eps = glie_a/(glie_a+ep)
+    else:
+    	eps = 0.01
+    cum_reward = 0
+
+    i = 0
+    if args.render:
+        env.render()
+    while not done:
+        # Select and perform an action
+        action = agent.get_action(state, eps)
+        next_state, reward, done, _ = env.step(action)
+        cum_reward += reward
+        next_state = agent._preprocess(next_state)
+
+        # Task 4: Update the DQN
+        agent.store_transition(state, action, next_state, reward, done)
+        agent.update_network()
+
+        # Move to the next state
+        state = next_state
+        if args.render:
+            env.render()
+        if done:
+            # EMPTY memory
+            #agent.history.empty()
+            if reward > 0:
+                wins += 1
+
+        i += 1
+    cumulative_rewards.append(cum_reward)
+    #plot_rewards(cumulative_rewards)
+    logging.info("Episode lasted for %i time steps", i)
+
+
+    # Update the target network, copying all weights and biases in DQN
+    # Uncomment for Task 4
+    if ep % TARGET_UPDATE == 0:
+        agent.update_target_network()
+
+    if ep % 10 == 0:
+        logging.info("trained for: %s episodes", ep)
+        logging.info("victory rate: %r", wins/(ep+1))
+        logging.info("Won total: %s",wins)
+
+    # Save the policy
+    # Uncomment for Task 4
+    if ep % 1000 == 0:
+        logging.info("saving model at ep: %s", ep)
+        torch.save(agent.policy_net.state_dict(), "weights_%s_%d_new.mdl" % ("DQN", ep))
+
+print('Complete')
